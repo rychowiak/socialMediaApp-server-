c529307d022891033043cd0116276ad17657d98c
ricardosf <ricardosepu93@gmail.com>
2023-01-05 09:57:08 -0700

    test connection

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use(
 );
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("APP IS RUNNING.");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
